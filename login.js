9a241cf2cfc4d721c5a98107524e56449fa13f1d
Roja1912 <41184330+Roja1912@users.noreply.github.com>
2018-07-23 15:33:59 +0530

    Add files via upload

--- login.js
+++ login.js
@@ -1,43 +1,15 @@
-window.onload=function() {
-	var button=document.getElementById("btn");
-	var name=document.getElementById("name");
-	var password=document.getElementById("password");
-	var isValid = function() {
-		console.log('clicked',name.value,password.value)
+window.onload=function(){
+var button=document.getElementById("btn");
+var name=document.getElementById("name");
+var password=document.getElementById("password");
+button.onclick=function(){
+console.log('clicked',name.value,password.value)
+if(name.value && password.value)
+{
+alert("login succesfully")
+}else{
+alert("please enter username and id")
+}
 
-                 var errNameRef=document.getElementById("name-err-js");
-	 	var errPasswordRef=document.getElementById("password-err-js");
-
-		var match=name.value.match(/[a-zA-Z0-9]+@+[a-zA-Z0-9]+\.[a-zA-Z]{2,3}/g);
-	      if(!match)
-			{
-
-			errNameRef.innerHTML="enter valid email";
-			return false;
-
-			}
-			else if(!password.value)
-			{
-			errNameRef.innerHTML="";
-			errPasswordRef.innerHTML="enter valid password";
-			return false;
-
-			}
-			else{
-			errNameRef.innerHTML=errPasswordRef.innerHTML="";
-			return true;
-			}
-		}
- 	name.onkeypress=function(){
-		isValid();
-	}
-	password.onkeypress=function(){
-		isValid();
-	}
-	button.onclick=function(){
-		if(isValid())
-		alert("login success");
-	localStorage.setItem('auth',name.value);
-	location.href="index.html"
-	}
+}
 }
